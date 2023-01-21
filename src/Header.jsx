5b5b4aded5fb78c783669584c664e2d9f402d462
Maxime Baude <maxime.baude0@gmail.com>
2023-01-21 18:47:53 +0100

    Ended interactiv list

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = ({ title }) => {
+  return (
+    <header>
+        <h1>{title}</h1>
+    </header>
+  )
+}
+
+/**
+ * Default values for the props
+ */
+Header.defaultProps = {
+    title: "Default title"
+}
+
+export default Header
